e48f206b0ed695a6a4f4efb3b92c9b4b540615b2
nam2626 <nam2626@naver.com>
2024-10-22 14:58:30 +0900

    게시판 - 기본키 설정

--- 20241022-board.sql
+++ 20241022-board.sql
@@ -49,8 +49,115 @@ create table board_comment_hate (
 	id	varchar2(50)		not null,
 	cno	number		not null
 );
+
 --2. 제약조건(기본키, 외래키)
 
+ALTER TABLE board_member ADD CONSTRAINT PK_BOARD_MEMBER 
+PRIMARY KEY (id);
+
+ALTER TABLE board ADD CONSTRAINT PK_BOARD PRIMARY KEY(bno);
+
+ALTER TABLE board_content_like 
+ADD CONSTRAINT PK_BOARD_CONTENT_LIKE PRIMARY KEY(bno,id);
+
+ALTER TABLE board_content_hate 
+ADD CONSTRAINT PK_BOARD_CONTENT_HATE PRIMARY KEY(bno,id);
+
+ALTER TABLE board_file 
+ADD CONSTRAINT PK_BOARD_FILE PRIMARY KEY (fno);
+
+ALTER TABLE board_comment 
+ADD CONSTRAINT PK_BOARD_COMMENT PRIMARY KEY(cno);
+
+ALTER TABLE board_comment_like 
+ADD CONSTRAINT PK_BOARD_COMMENT_LIKE PRIMARY KEY (id,cno);
+
+ALTER TABLE board_comment_hate 
+ADD CONSTRAINT PK_BOARD_COMMENT_HATE PRIMARY KEY (id,cno);
+
+ALTER TABLE board ADD CONSTRAINT FK_board_member_TO_board_1 FOREIGN KEY (
+	id
+)
+REFERENCES board_member (
+	id
+);
+
+ALTER TABLE board_content_like ADD CONSTRAINT FK_board_TO_board_content_like_1 FOREIGN KEY (
+	bno
+)
+REFERENCES board (
+	bno
+);
+
+ALTER TABLE board_content_like ADD CONSTRAINT FK_board_member_TO_board_content_like_1 FOREIGN KEY (
+	id
+)
+REFERENCES board_member (
+	id
+);
+
+ALTER TABLE board_content_hate ADD CONSTRAINT FK_board_TO_board_content_hate_1 FOREIGN KEY (
+	bno
+)
+REFERENCES board (
+	bno
+);
+
+ALTER TABLE board_content_hate ADD CONSTRAINT FK_board_member_TO_board_content_hate_1 FOREIGN KEY (
+	id
+)
+REFERENCES board_member (
+	id
+);
+
+ALTER TABLE board_file ADD CONSTRAINT FK_board_TO_board_file_1 FOREIGN KEY (
+	bno
+)
+REFERENCES board (
+	bno
+);
+
+ALTER TABLE board_comment ADD CONSTRAINT FK_board_TO_board_comment_1 FOREIGN KEY (
+	bno
+)
+REFERENCES board (
+	bno
+);
+
+ALTER TABLE board_comment ADD CONSTRAINT FK_board_member_TO_board_comment_1 FOREIGN KEY (
+	id
+)
+REFERENCES board_member (
+	id
+);
+
+ALTER TABLE board_comment_like ADD CONSTRAINT FK_board_member_TO_board_comment_like_1 FOREIGN KEY (
+	id
+)
+REFERENCES board_member (
+	id
+);
+
+ALTER TABLE board_comment_like ADD CONSTRAINT FK_board_comment_TO_board_comment_like_1 FOREIGN KEY (
+	cno
+)
+REFERENCES board_comment (
+	cno
+);
+
+ALTER TABLE board_comment_hate ADD CONSTRAINT FK_board_member_TO_board_comment_hate_1 FOREIGN KEY (
+	id
+)
+REFERENCES board_member (
+	id
+);
+
+ALTER TABLE board_comment_hate ADD CONSTRAINT FK_board_comment_TO_board_comment_hate_1 FOREIGN KEY (
+	cno
+)
+REFERENCES board_comment (
+	cno
+);
 --3. 시퀸스 생성
 
 --4. 샘플 데이터 저장
