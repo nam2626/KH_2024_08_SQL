e65d54eb41b34ff1633f47aaa29f8b5cc255ca94
nam2626 <nam2626@naver.com>
2024-10-25 09:49:35 +0900

    프로시저 기본 예제

--- 20241024.sql
+++ 20241024.sql
@@ -290,3 +290,29 @@ DELETE FROM BOARD WHERE BNO = 999999;
 UPDATE BOARD SET TITLE = '제목2', CONTENT = '내용2' WHERE BNO = 999999;
 
 SELECT * FROM BOARD_LOG;
+
+-----------------------------------------------------------------------
+-- 프로시저
+-- 	  SQL 쿼리문으로 로직을 조합해서 사용하는 데이터베이스 코드
+--	  SQL문과 제어문을 이용해서, 데이터를 검색, 삽입, 수정, 삭제를 할수 있음
+--	  결과를 외부로 전달할 수도 있음.
+--    하나의 트랜잭션 구성시 사용을 함.
+-----------------------------------------------------------------------
+--	매개변수가 없는 프로시저
+CREATE OR REPLACE PROCEDURE PROCEDURE_EX1
+IS
+	--변수 선언
+	TEST_VAR VARCHAR2(100);
+BEGIN
+	--실행부
+	TEST_VAR := 'HELLO WORLD';
+	DBMS_OUTPUT.PUT_LINE(TEST_VAR);	
+END;
+
+SET SERVEROUTPUT ON;
+
+--프로시저 실행되는 부분
+BEGIN
+	--실행부
+	PROCEDURE_EX1();
+END;
